8dd8a96a58f6a62ce84c86cf0f3571dbcab26ac5
GREEN481 <ivaninmail@yandex.ru>
2025-05-02 01:08:35 +0300

    Add files via upload

---
+++ Diplom/js/index.js
@@ -0,0 +1,65 @@
+(function () {
+  
+  // Модалка
+
+  const modal = document.querySelector('.modal')
+  const modalButton = document.querySelector('.hero__consultation')
+
+  modalButton.addEventListener('click', openModal)
+  modal.addEventListener('click', closeModal)
+
+  function openModal(e) {
+    e.preventDefault()
+    document.body.classList.toggle('body--opened-modal')
+  }
+
+  function closeModal(e) {
+    e.preventDefault()
+
+    const target = e.target
+
+    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+      document.body.classList.remove('body--opened-modal')
+    }
+
+  }
+  // Маска для телефона
+
+  const telInputs = document.querySelectorAll('input[type="tel"]')
+  const im = new Inputmask('+7(999) 999-99-99')
+  im.mask(telInputs)
+
+    // Аккордион
+    
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+  accordionLists.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+
+      const accordionList = e.currentTarget
+      const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+      const accordionControl = e.target.closest('.accordion-list__control')
+      if (!accordionControl) return
+      e.preventDefault()
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove('accordion-list__item--opened')
+        accordionOpenedContent.style.maxHeight = null
+      }
+      accordionItem.classList.toggle('accordion-list__item--opened')
+
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null
+      }
+
+    })
+  })
+
+})()
